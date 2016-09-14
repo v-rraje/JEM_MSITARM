293e481ca7e43a75bacf911fa68547c1ccd59de5
v-rraje <v-rraje@microsoft.com>
2016-09-14 20:39:06 +0530

    Update CopySqlBackupfiles.ps1

--- CopySqlBackupfiles.ps1
+++ CopySqlBackupfiles.ps1
@@ -1,8 +1,8 @@
-﻿ Param
+ Param
 (
 [Parameter(Mandatory=$true)]
 [String]
 $sqlServer
 )
 
-Robocopy  "\\azcujemdeviis02\Jem_ARM_V6_Bits\DataBase" "$sqlServer\E$\MSSQL11.MSSQLSERVER\MSSQL\Backup" /e
+Robocopy  "\\azcujemdeviis02\Jem_ARM_V6_Bits\DataBase" "\\$sqlServer\E$\MSSQL11.MSSQLSERVER\MSSQL\Backup" /e
